0094edc8ac145d2e50a7c87da79b203eb8e4d4bd
Isiah Jones <jones.isiah.blhs@gmail.com>
2021-07-06 14:11:44 -0400

    update main.js

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
-const themeToggle = document.querySelector("#theme-toggle");
+//
+const themeToggle = document.querySelector("#theme-toggle")
 
 themeToggle.addEventListener("click", () => {
   document.body.classList.contains("light-theme")
     ? enableDarkMode()
-    : enableLightMode();
-});
+    : enableLightMode()
+})
 
 function enableDarkMode() {
-  document.body.classList.remove("light-theme");
-  document.body.classList.add("dark-theme");
-  themeToggle.setAttribute("aria-label", "Switch to light theme");
+  document.body.classList.remove("light-theme")
+  document.body.classList.add("dark-theme")
+  themeToggle.setAttribute("aria-label", "Switch to light theme")
 }
 
 function enableLightMode() {
-  document.body.classList.remove("dark-theme");
-  document.body.classList.add("light-theme");
-  themeToggle.setAttribute("aria-label", "Switch to dark theme");
+  document.body.classList.remove("dark-theme")
+  document.body.classList.add("light-theme")
+  themeToggle.setAttribute("aria-label", "Switch to dark theme")
 }
 
 function setThemePreference() {
   if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    enableDarkMode();
-    return;
+    enableDarkMode()
+    return
   }
-  enableLightMode();
+  enableLightMode()
 }
 
-document.onload = setThemePreference();
+document.onload = setThemePreference()
